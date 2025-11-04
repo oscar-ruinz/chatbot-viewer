370cd52c0701a9bcc599a8c74c266aded31aed57
Oscar Ruelas <oscar.ruelasinz1999@gmail.com>
2025-11-03 21:35:51 -0700

    update api url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import ChatbotConversationViewer from './ChatbotConversationViewer'
 
 export default function App() {
-  return <ChatbotConversationViewer apiBase="http://localhost:3000/api" />
+  return <ChatbotConversationViewer apiBase="https://pg-api.chachin.fun/api" />
 }
